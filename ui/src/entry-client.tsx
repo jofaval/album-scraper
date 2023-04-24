030b69fb553012497496fd3dd99e95cf270c4333
pepe_fabra_valverde <jofaval@outlook.com>
2023-04-24 20:36:17 +0200

    docs(ui): add index retrieval proposal

--- ui/src/entry-client.tsx
+++ ui/src/entry-client.tsx
@@ -1,5 +1,7 @@
 import { mount, StartClient } from "solid-start/entry-client";
 
 // TODO: multi-lang options?
+// TODO: implement a slug for index retrieval from the URL, no function should be passed there (eval should be avoided)
+// so maybe it would be a viable solution to parse a string like /album-chapter-{index}-...
 
 mount(() => <StartClient />, document);
